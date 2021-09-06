69635143b057ef14a1a01d6dd3caa794d4083c12
董少强 <2060214611@qq.com>
2021-09-06 17:29:44 +0800

    1
    1

--- 前端/node-server/config/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  port: process.env.PORT || 3000, // 项目启动端口
-  apiPrefix: '/api', // 配置接口的统一请求前缀
-  database: 'mongodb://localhost:27017/test', // 开发环境
-  databaseProd: 'mongodb://localhost:27017/test', // 生成环境需要提供mongodb的用户名和密码
-}
